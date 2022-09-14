7873c9d2c7af0742359215e48a10af7d17e8b216
Andrew <Andrew Jarvis>
2022-09-13 22:00:48 -0400

    Initial bootstrap script

---
+++ bootstrap.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+echo "Lift yourself up by your bootstraps"
+# TODO: install git
+# TODO: prompt for email for git global config
+# TODO: prompt for name for git global config
+# TODO: confirm email/name
+
