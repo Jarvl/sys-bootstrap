c2fb2b54ad81fc070aa4720ca15cae0705f40e9f
Andrew Jarvis <ajarvis7771@gmail.com>
2022-09-15 02:29:06 -0400

    Fix script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -euxo pipefail
+set -euo pipefail
 
 cache_dir=.cache
 
@@ -54,15 +54,24 @@ configure_git() {
   echo "Public key contents:"
   cat "$private_key_filename.pub"
 
-  # TODO: Ask "Would you like to test your Github SSH connection?"
-
   echo ""
   read -p "Add public key to Github, then press any button to test SSH connection" -n 1
 
   # Will return with exit code 1 when successfully authenticated
   while ssh -T git@github.com; [[ $? -ne 1 ]]; do
     echo ""
-    read -p "SSH connection to Github failed, press any button to try again" -n 1
+    echo "Connection failed, would you like to try again?"
+
+    select yn in "Yes" "No"; do
+      case $yn in
+        Yes ) echo "Attempting connection..."; break;;
+        No ) break;;
+      esac
+    done
+
+    if [[ $yn = "No" ]]; then
+      break
+    fi
   done
 
   echo ""
@@ -87,7 +96,6 @@ install_docker() {
   echo ""
   echo "Verifying docker installation"
   sudo service docker start
-  # Workaround because newgrp starts a new shell
   sudo docker run hello-world
 
   sudo systemctl enable docker.service
@@ -96,26 +104,31 @@ install_docker() {
   # From https://docs.docker.com/engine/install/linux-postinstall/
   sudo getent group docker || sudo groupadd docker
   sudo usermod -aG docker $(whoami)
-  # Suppress errors about .docker directory not existing
-  sudo chown "$(whoami)":"$(whoami)" /home/"$(whoami)"/.docker -R 2>/dev/null
-  sudo chmod g+rwx "/home/$(whoami)/.docker" -R 2>/dev/null
+  # allow nonroot access to docker and suppress errors about .docker directory not existing
+  docker_dotdir=/home/"$(whoami)"/.docker
+  sudo chown -R "$(whoami)":"$(whoami)" $docker_dotdir || echo "$docker_dotdir does not exist"
+  sudo chmod -R g+rwx $docker_dotdir || echo "$docker_dotdir does not exist"
 } && cache_func_call install_docker
 
 install_neovim() {
   curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
   chmod u+x nvim.appimage
   ./nvim.appimage --appimage-extract > /dev/null 2>&1
-  cp ./squashfs-root/AppRun /usr/bin/nvim
+  sudo cp ./squashfs-root/AppRun /usr/bin/nvim
 } && cache_func_call install_neovim
 
 install_zsh() {
   sudo apt install -y zsh
   zsh --version
-  chsh -s $(which zsh)
+  sudo chsh -s $(which zsh) $(whoami)
 } && cache_func_call install_zsh
 
 install_ohmyzsh() {
-  sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+  # Force ohmyzsh to not switch shells during script execution
+  yes no | sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" || ohmyzsh_ret=$?
+  if [[ $ohmyzsh_ret -ne 141 ]]; then
+    exit $ohmyzsh_ret
+  fi
 } && cache_func_call install_ohmyzsh
 
 configure_ohmyzsh() {
@@ -128,25 +141,25 @@ install_asdf() {
 } && cache_func_call install_asdf
 
 install_asdf_python() {
-  asdf plugin-add python
+  asdf plugin-add python || echo ""
   sudo apt install -y make build-essential libssl-dev zlib1g-dev \
                             libbz2-dev libreadline-dev libsqlite3-dev llvm \
                             libncursesw5-dev xz-utils tk-dev libxml2-dev \
-                            libxmlsec1-dev libffi-dev liblzma-dev\
+                            libxmlsec1-dev libffi-dev liblzma-dev
   read -p "Enter python version and press [ENTER]: " asdf_python_version
   asdf install python $asdf_python_version
   asdf global python $asdf_python_version
 } && cache_func_call install_asdf_python
 
 install_asdf_nodejs() {
-  asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
+  asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git || echo ""
   read -p "Enter Node.js version and press [ENTER]: " asdf_nodejs_version
   asdf install nodejs $asdf_nodejs_version
   asdf global nodejs $asdf_nodejs_version
 } && cache_func_call install_asdf_nodejs
 
 install_asdf_rust() {
-  plugin-add rust https://github.com/asdf-community/asdf-rust.git
+  asdf plugin-add rust https://github.com/asdf-community/asdf-rust.git || echo ""
   read -p "Enter Rust version and press [ENTER]: " asdf_rust_version
   asdf install rust $asdf_rust_version
   asdf global rust $asdf_rust_version
@@ -161,66 +174,65 @@ install_kde_plasma() {
 } && cache_func_call install_kde_plasma
 
 echo ""
-echo "Lift yourself up by your bootstraps"
+echo "Lift yourself up by your bootstraps."
 
 echo ""
-echo "Updating apt"
+echo "Updating apt."
 update_apt
 
 echo ""
-echo "Installing git"
+echo "Installing git."
 install_git
 
 echo ""
-echo "Configuring git"
+echo "Configuring git."
 configure_git
 
 echo ""
-echo "Installing docker"
+echo "Installing docker."
 install_docker
 
 echo ""
-echo "Installing neovim"
+echo "Installing neovim."
 install_neovim
 
 echo ""
-echo "Installing zsh and setting as default shell (requires logout)"
+echo "Installing zsh and setting as default shell (requires logout)."
 install_zsh
 
 echo ""
-echo "Installing OhMyZsh"
+echo "Installing OhMyZsh."
 install_ohmyzsh
 
 echo ""
-echo "Configuring OhMyZsh"
+echo "Configuring OhMyZsh."
 configure_ohmyzsh
 
 echo ""
-echo "Installing asdf package manager"
+echo "Installing asdf package manager."
 install_asdf
 
 echo ""
-echo "Installing python via asdf"
+echo "Installing python via asdf."
 install_asdf_python
 
 echo ""
-echo "Installing Node.js via asdf"
+echo "Installing Node.js via asdf."
 install_asdf_nodejs
 
 echo ""
-echo "Installing Rust via asdf"
+echo "Installing Rust via asdf."
 install_asdf_rust
 
 echo ""
-echo "Installing thefuck alias"
+echo "Installing thefuck alias."
 install_thefuck_alias
 
 echo ""
-echo "Installing KDE Plasma"
+echo "Installing KDE Plasma."
 install_kde_plasma
 
 # TODO: install lunarvim
-# TODO: prompt for reboot y/n
 
 echo ""
-echo "Done"
+echo "Done bootstrapping. Please reboot."
